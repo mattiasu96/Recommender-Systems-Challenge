7abaa2d910ac98a3b607c78c36b44133de97b4d5
Mattia <mattiasu96@live.it>
2020-11-21 15:27:47 +0100

    Added hybrid with normalization + some tests. Nothing new

--- KNN/ItemKNNScoresHybridRecommender_Normalized.py
+++ KNN/ItemKNNScoresHybridRecommender_Normalized.py
@@ -9,17 +9,17 @@ Created on 15/11/20
 from Base.Recommender_utils import check_matrix, similarityMatrixTopK
 from Base.BaseSimilarityMatrixRecommender import BaseItemSimilarityMatrixRecommender
 
-class ItemKNNScoresHybridRecommender(BaseItemSimilarityMatrixRecommender):
+class ItemKNNScoresHybridRecommender_Normalized(BaseItemSimilarityMatrixRecommender):
     """ ItemKNNScoresHybridRecommender
     Hybrid of two prediction scores R = R1*alpha + R2*(1-alpha)
 
     """
 
-    RECOMMENDER_NAME = "ItemKNNScoresHybridRecommender"
+    RECOMMENDER_NAME = "ItemKNNScoresHybridRecommender_Normalized"
 
 
     def __init__(self, URM_train, Recommender_1, Recommender_2):
-        super(ItemKNNScoresHybridRecommender, self).__init__(URM_train)
+        super(ItemKNNScoresHybridRecommender_Normalized, self).__init__(URM_train)
 
         self.URM_train = check_matrix(URM_train.copy(), 'csr')
         self.Recommender_1 = Recommender_1
@@ -34,7 +34,16 @@ class ItemKNNScoresHybridRecommender(BaseItemSimilarityMatrixRecommender):
         
         item_weights_1 = self.Recommender_1._compute_item_score(user_id_array)
         item_weights_2 = self.Recommender_2._compute_item_score(user_id_array)
+
         print('shape of item_weights:', item_weights_1.shape)
+        item_weights_1_max = item_weights_1.max()
+        item_weights_2_max = item_weights_2.max()
+
+        if not item_weights_1.any() == 0:
+            item_weights_1 /= item_weights_1_max
+        if not item_weights_2.any() == 0:
+            item_weights_2 /= item_weights_2_max
+
 
         item_weights = item_weights_1*self.alpha + item_weights_2*(1-self.alpha)
 
